81ef8d015ed0248c14dc9e36d8d0f4a1c4501221
04PR33 <63194092+04PR33@users.noreply.github.com>
2020-04-12 16:42:12 +0300

    Update Custom.cpp

--- Custom.cpp
+++ Custom.cpp
@@ -28,5 +28,5 @@ string Custom::getdescription()
 
 string Custom::toString()
 {
-	return "{date: '"+to_string(date.getDay())+"."+ to_string(date.getMonth())+ "." + to_string(date.getYear()) + "'\ntime: '"+to_string(time.getHour())+":"+ to_string(time.getMinutes) +":"+ to_string(time.getSeconds()) +"'\ndescription: '"+this->description+"'}";
+	return "{date: '"+to_string(date.getDay())+"."+ to_string(date.getMonth())+ "." + to_string(date.getYear()) + "'\ntime: '"+to_string(time.getHour())+":"+ to_string(time.getMinutes()) +":"+ to_string(time.getSeconds()) +"'\ndescription: '"+this->description+"'}";
 }
