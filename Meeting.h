3182084c445f124472302caa99c986c49bb4c957
04PR33 <nikita.nychyporchuk@gmail.com>
2020-04-12 16:33:36 +0300

    Nychyporchuk

--- Meeting.h
+++ Meeting.h
@@ -10,5 +10,20 @@ class Meeting :
 protected:
 	string place;
 	string person;
+
+public:
+	Meeting();
+	Meeting(Date d, Time_ t, string place, string person);
+	~Meeting();
+
+	void setplace(string place);
+	void setperson(string person);
+	string getplace()const&;
+	string getperson()const&;
+	 
+		
+	virtual void show();
+	virtual string type();
+
 };
 
