3b5dcf146671011cd61d9ef0708fbd5581bafec4
FoFo-Admin <denis25032014@gmail.com>
2020-04-12 16:23:15 +0300

    Khramtsov D.I.

--- Custom.cpp
+++ Custom.cpp
@@ -1,5 +1,21 @@
 #include "Custom.h"
 
+Custom::Custom()
+{
+	this->date = Date();
+	this->time = Time_();
+	this->description = "Some event";
+}
+
+Custom::Custom(string description, Date date, Time_ time) : Event(date, time)
+{
+	this->description = description;
+}
+
+Custom::~Custom()
+{
+}
+
 string Custom::toString()
 {
 	return "{date: '"+to_string(date.getDay())+"."+ to_string(date.getMonth())+ "." + to_string(date.getYear()) + "'\ntime: '"+to_string(time.getHour())+":"+ to_string(time.getMinutes) +":"+ to_string(time.getSeconds()) +"'\ndescription: '"+this->description+"'}";
